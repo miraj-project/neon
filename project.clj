b4627865dc9accd448a248bd8390c47e8cc95bb9
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-10 19:23:25 -0600

    initial commit polymer 1.2.3 neon elements

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject miraj.polymer/neon "1.2.3-SNAPSHOT"
+  :description "Polymer Neon Elements"
+  :url "https://github.com/mobileink/polymer.neon"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"})
