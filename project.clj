262f8fab067996fa7a2446dba4c34b123151b16c
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 13:09:12 -0600

    project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject polymer/neon "1.2.3-SNAPSHOT"
-  :description "Polymer Neon markup functions"
+(defproject miraj/neon "1.2.3-SNAPSHOT"
+  :description "Miraj polymer Neon markup functions"
   :url "https://github.com/mobileink/polymer.neon"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [miraj/markup "0.1.0-SNAPSHOT"]])
